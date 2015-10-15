596606bbbe8baf399e5cc2ec18aac2e19a76da13
kiruthika selvavinayagam <kiruthika.selvavinayagam@olacabs.com>
2015-10-15 16:23:38 +0530

    Initial Commit

---
+++ ITRFlipView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ITRFlipView
+//
+//  Created by kiruthika selvavinayagam on 10/15/15.
+//  Copyright © 2015 kiruthika selvavinayagam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
